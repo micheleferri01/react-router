46dbcd2b0331772d6387bde05b46c1f3bc6c9867
micheleferri01 <micheleferri095@gmail.com>
2026-02-18 18:28:19 +0100

    edit: font size del testo nella navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
             <div className="container-fluid fs-5">
-                <Link className="navbar-brand" to="/">E-shop</Link>
+                <Link className="navbar-brand fs-3" to="/">E-shop</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon text-white"></span>
                 </button>
