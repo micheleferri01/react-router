ed7e9e16e6119b2afe8d4c0b4cbff864f10e280f
micheleferri01 <micheleferri095@gmail.com>
2026-02-18 17:45:23 +0100

    add: products page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import DefaultLayout from "./layouts/DefaultLayout"
 import HomePage from "./pages/HomePage"
+import ProductsPage from "./pages/ProductsPage"
 
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
         <Routes>
           <Route Component={DefaultLayout}>
             <Route path="/" Component={HomePage}/>
+            <Route path="/products" Component={ProductsPage}/>
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,41 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+export default function ProductsPage() {
+
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        axios.get("https://fakestoreapi.com/products")
+            .then((res) => {
+                setProducts(res.data)
+                console.log(res.data)
+            })
+    }, [])
+
+    return (
+        <>
+            <div className="container-fluid py-4 bg-orange">
+                <div className="row g-3">
+                    {products.map((product) => (
+                        <div key={product.id} className="col-3">
+                            <div className="card pt-3 h-100">
+                                <div className="productCard-image mx-auto">
+                                    <img src={product.image} alt={product.title} />
+                                </div>
+                                <div className="card-body d-flex flex-column justify-content-between">
+                                    <h1 className="fs-5 card-title align-self-center">{product.title}</h1>
+                                    <div className="d-flex justify-content-between">
+                                        <p className="fs-4 align-self-end">{product.rating.rate}<span className="star-color">&#9733;</span></p>
+                                        <p className="d-flex flex-column">prezzo <span className="fs-4">{product.price.toFixed(2)} &euro;</span></p>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>))}
+                </div>
+            </div>
+        </>
+
+    )
+}
