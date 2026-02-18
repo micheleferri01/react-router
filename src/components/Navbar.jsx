ec5eb6268f42acf848ebb23f8642d9393393673f
micheleferri01 <micheleferri095@gmail.com>
2026-02-18 18:17:57 +0100

    edit: stile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,22 @@ import { Link, NavLink } from "react-router-dom"
 
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg bg-dark">
+        <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
             <div className="container-fluid fs-5">
-                <Link className="navbar-brand text-white" to="/">E-shop</Link>
+                <Link className="navbar-brand" to="/">E-shop</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon text-white"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <NavLink className="nav-link text-white" to="/">Home</NavLink>
+                            <NavLink className="nav-link" to="/">Home</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link text-white" to="/products">Prodotti</NavLink>
+                            <NavLink className="nav-link" to="/products">Prodotti</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link text-white" to="/about-us">Chi siamo</NavLink>
+                            <NavLink className="nav-link" to="/about-us">Chi siamo</NavLink>
                         </li>
                     </ul>
                 </div>
