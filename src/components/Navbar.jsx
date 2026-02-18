860e6259c6d6954d72d366d651b827a994491c47
micheleferri01 <micheleferri095@gmail.com>
2026-02-18 16:23:51 +0100

    edit: nomi dei link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,20 +5,20 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg bg-dark">
             <div className="container-fluid fs-5">
-                <Link className="navbar-brand text-white" to="/">Navbar</Link>
+                <Link className="navbar-brand text-white" to="/">E-shop</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon text-white"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <NavLink className="nav-link active text-white" to="/">Home</NavLink>
+                            <NavLink className="nav-link text-white" to="/">Home</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link text-white" to="/products">Products</NavLink>
+                            <NavLink className="nav-link text-white" to="/products">Prodotti</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink className="nav-link text-white" to="/about-us">About Us</NavLink>
+                            <NavLink className="nav-link text-white" to="/about-us">Chi siamo</NavLink>
                         </li>
                     </ul>
                 </div>
