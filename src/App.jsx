fcf94bd6135bd30499e4b899b4ad6290cd789277
micheleferri01 <micheleferri095@gmail.com>
2026-02-18 15:57:51 +0100

    add: layout temporaneo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import DefaultLayout from "./layouts/DefaultLayout"
+import HomePage from "./pages/HomePage"
 
 
 export default function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route path="/" Component={HomePage}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
