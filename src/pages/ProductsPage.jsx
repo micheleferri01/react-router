42dc781be359731185d8fc791156eeb43a226661
micheleferri01 <micheleferri095@gmail.com>
2026-02-19 16:18:39 +0100

    create: pagina dei dettagli prodotto

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
 export default function ProductsPage() {
 
@@ -17,29 +18,33 @@ export default function ProductsPage() {
 
     return (
         <>
-        {error && <>
-        <div className="alert alert-danger my-3 text-center">Si è verificato un error durante il caricamento. Riprova.</div>
-        </>}
+            {error && <>
+                <div className="alert alert-danger my-3 text-center">Si è verificato un error durante il caricamento. Riprova.</div>
+            </>}
             {error === false && <div className="container-fluid py-4 bg-orange">
-                <div className="row g-3">
+                <div className="row g-3 row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5">
                     {products.map((product) => (
-                        <div key={product.id} className="col-3">
-                            <div className="card pt-3 h-100">
-                                <div className="productCard-image mx-auto">
-                                    <img src={product.image} alt={product.title} />
-                                </div>
-                                <div className="card-body d-flex flex-column justify-content-between">
-                                    <h1 className="fs-5 card-title align-self-center">{product.title}</h1>
-                                    <div className="d-flex justify-content-between">
-                                        <p className="fs-4 align-self-end">{product.rating.rate}<span className="star-color">&#9733;</span></p>
-                                        <p className="d-flex flex-column">prezzo <span className="fs-4">{product.price.toFixed(2)} &euro;</span></p>
+                        <div key={product.id} className="col">
+                            <Link to={`/products/${product.id}`} className="text-decoration-none">
+                                <div className="card pt-3 h-100">
+                                    <div className="productCard-image mx-auto">
+                                        <img src={product.image} alt={product.title} />
+                                    </div>
+                                    <div className="card-body d-flex flex-column justify-content-between">
+                                        <h1 className="fs-5 card-title align-self-center">{product.title}</h1>
+                                        <div className="d-flex justify-content-between">
+                                            <p className="fs-4 align-self-end">{product.rating.rate}<span className="star-color">&#9733;</span></p>
+                                            <p className="d-flex flex-column">prezzo <span className="fs-4">{product.price.toFixed(2)} &euro;</span></p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </Link>
+
+
 
                         </div>))}
                 </div>
-            </div>}
+            </div >}
         </>
 
     )
