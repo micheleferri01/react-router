a5ea2dc2a954023ecef7521849fdc2127a3862ca
micheleferri01 <micheleferri095@gmail.com>
2026-02-19 17:16:38 +0100

    add: card dei dettagli prodotto

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -8,12 +8,41 @@ export default function ProductDetailsPage() {
 
     const fetchProduct = () => {
         axios.get(`https://fakestoreapi.com/products/${id}`)
-            .then((res) => { console.log(res.data)
-                setProduct(res.data)
-             });
+            .then((res) => {
+                console.log(res.data);
+                setProduct(res.data);
+            });
     }
 
     useEffect(fetchProduct, []);
 
-    return <h1>Details Page {id}</h1>
+    if(!product) return <h1>Loading...</h1>
+
+    return (
+        <>
+            <div className="container py-5">
+                <div className="bg-white p-4 rounded-5">
+                    <div className="d-flex gap-5">
+                        <div>
+                            <h1 className="mb-3">{product.title}</h1>
+
+                            <p><strong>Categoria:</strong> {product.category.charAt(0).toUpperCase() + product.category.slice(1)}</p>
+                            <p><strong>Descrizione:</strong> {product.description}</p>
+                            <div className="d-flex justify-content-between">
+
+                                <p>Prezzo<span className="fs-2">{product.price.toFixed(2)} &euro;</span></p>
+                                <p>Rating <span className="fs-2">{product.rating.rate}<span className="star-color">&#9733;</span></span></p>
+
+                            </div>
+
+                        </div>
+                        <div className="productDetailImage">
+                            <img src={product.image} alt={product.title} />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
 }
