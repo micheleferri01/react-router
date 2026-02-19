7fb9f703c4b111fd7ac4921097e65be4f62138e1
micheleferri01 <micheleferri095@gmail.com>
2026-02-19 19:16:28 +0100

    add: comportamento in caso il prodotto non viene trovato

--- src/pages/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage.jsx
@@ -1,29 +1,44 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 
 export default function ProductDetailsPage() {
     const { id } = useParams();
     const [product, setProduct] = useState();
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState();
+
+    const navigate = useNavigate();
 
     const fetchProduct = () => {
+        setIsLoading(true);
+
         axios.get(`https://fakestoreapi.com/products/${id}`)
             .then((res) => {
+
+                if (!res.data || Object.keys(res.data).length === 0) {
+                    throw new Error('Prodotto non trovato');
+                }
+
                 console.log(res.data);
                 setProduct(res.data);
-            });
+            })
+            .catch((error) => {
+                setError(error);
+            })
+            .finally(() => { setIsLoading(false) });
     }
 
     useEffect(fetchProduct, []);
 
-    if (!product) return (
+    if (isLoading) return (
         <div className="loading-screen">
             <h1>Loading...</h1>
         </div>
-        )
+    )
 
-    return (
-        <>
+     return (
+        error ? navigate('/products') :  <>
             <div className="container py-5">
                 <div className="bg-white p-4 rounded-5">
                     <div className="d-flex gap-5">
@@ -31,14 +46,13 @@ export default function ProductDetailsPage() {
                             <h1 className="mb-3">{product.title}</h1>
 
                             <p><strong>Categoria:</strong> {product.category.charAt(0).toUpperCase() + product.category.slice(1)}</p>
+
                             <p><strong>Descrizione:</strong> {product.description}</p>
+
                             <div className="d-flex justify-content-between">
-
-                                <p>Prezzo<span className="fs-2">{product.price.toFixed(2)} &euro;</span></p>
+                                <p>Prezzo <span className="fs-2">{product.price.toFixed(2)} &euro;</span></p>
                                 <p>Rating <span className="fs-2">{product.rating.rate}<span className="star-color">&#9733;</span></span></p>
-
                             </div>
-
                         </div>
                         <div className="productDetailImage">
                             <img src={product.image} alt={product.title} />
@@ -48,5 +62,6 @@ export default function ProductDetailsPage() {
                 </div>
             </div>
         </>
+       
     )
 }
