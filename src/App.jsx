65bbb06b98bebd68e6cf28fe69f6516102e0c89f
micheleferri01 <micheleferri095@gmail.com>
2026-02-19 18:58:31 +0100

    add: Pagina 404

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from "./pages/HomePage"
 import ProductsPage from "./pages/ProductsPage"
 import AboutUs from "./pages/AboutUs"
 import ProductDetailsPage from "./pages/ProductDetailsPage"
+import NotFoundPage from "./pages/NotFoundPage"
 
 
 export default function App() {
@@ -17,6 +18,7 @@ export default function App() {
             <Route path="/about-us" Component={AboutUs}/>
             <Route path="/products" Component={ProductsPage}/>
             <Route path="/products/:id" Component={ProductDetailsPage}/>
+            <Route path="*" Component={NotFoundPage}/>
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,8 @@
+
+export default function NotFoundPage() {
+    return (
+        <div className="container my-5">
+            <h1 className="text-center">404 - Page Not Found</h1>
+        </div>
+    )
+}
